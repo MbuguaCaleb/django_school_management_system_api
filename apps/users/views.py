fc600d7b7863b44b0c0da11041187e64e3c51441
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-03-02 06:47:33 +0300

    m-finishing-up-user-model-and-set-up

--- apps/users/views.py
+++ apps/users/views.py
@@ -6,9 +6,10 @@ from django.contrib.auth import get_user_model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()  # returns User
-        fields = ('email', 'password', 'is_student')
+        fields = ('email', 'password')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
 
+    # Overriding the default create model
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
 
